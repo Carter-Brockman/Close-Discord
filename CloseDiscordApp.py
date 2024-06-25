d43230817feb0ab6f51f5cc99b8acf7fc64b2cac
Carter Brockman <carteribrockman@gmail.com>
2024-06-25 17:24:01 -0400

    For anyone who has issues with discord not working, this will quickly fix the issue NOTE: only for windows users

---
+++ CloseDiscordApp.py
@@ -0,0 +1,28 @@
+import os
+import time
+
+programIsRunning = True
+counter = 0
+
+while (programIsRunning == True) and (counter <= 2):
+
+    userChoice = input("End Discord? (y/n): ")
+    userChoice = userChoice.upper()
+
+
+    if (userChoice == 'Y'):
+        os.system("taskkill /f /IM Discord.exe")
+        programIsRunning = False
+
+    elif (userChoice == 'N'):
+        programIsRunning = False
+
+    else:
+        os.system("cls")
+        print("\nWrong input, type either 'y' or 'n'")
+        counter += 1
+        programIsRunning = True
+if counter >= 3:
+    print("\nToo many wrong inputs...\nClosing program")
+    time.sleep(3)
+    exit(0)
