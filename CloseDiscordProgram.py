44959ce1ffd1b4a1e47edc725888ba02dcb66908
Carter Brockman <carteribrockman@gmail.com>
2024-06-27 16:08:56 -0400

    For anyone who has issues with discord not working, this will quickly fix the issue NOTE: only for windows users Update: Now has a GUI for simplicity

--- CloseDiscordProgram.py
+++ CloseDiscordProgram.py
@@ -1,35 +1,40 @@
+import tkinter as tk
+from tkinter import messagebox
 import os
-import time
-
-programIsRunning = True
-counter = 0
 
 
-while (programIsRunning == True) and (counter <= 2):
-
-    if counter >= 1:
-        print('')
-
-    userChoice = input("End Discord? (y/n): ")
-    userChoice = userChoice.upper()
+discordIsOpen = True
 
 
-    if (userChoice == 'Y'):
-        os.system("taskkill /f /IM Discord.exe")
-        programIsRunning = False
+class closeDiscordGUI:
 
-    elif (userChoice == 'N'):
-        programIsRunning = False
+    def __init__(self):
 
-    else:
-        os.system("cls")
-        print("Wrong input, type either 'y' or 'n'")
-        counter += 1
-        programIsRunning = True
+        self.root = tk.Tk()
+
+        self.root.geometry("700x500")
+        self.root.title("Close Discord")
+
+        self.label = tk.Label(self.root, text="Close Discord", font=('arial', 24))
+        self.label.pack(padx=0, pady=10)
+
+        self.closeDiscordButton = tk.Button(self.root, text="Close Discord", font=('Arial', 34), command=killDiscord)
+        self.closeDiscordButton.place(x=150, y=125, height=200, width=400)
+
+        self.exitProgramButton = tk.Button(self.root, text="Exit Program", font=('Arial', 20), command=closeProgram)
+        self.exitProgramButton.place(x=250, y=350, height=100, width=200)
+
+        self.root.mainloop()
 
 
-if counter >= 3:
-    os.system("cls")
-    print("Too many incorrect inputs...\nClosing program")
-    time.sleep(3)
+def killDiscord():
+    os.system("taskkill /f /IM Discord.exe")
+    messagebox.showinfo(title="Discord Closed", message="Discord has been closed")
     exit(0)
+
+
+def closeProgram():
+    exit(0)
+
+
+closeDiscordGUI()
